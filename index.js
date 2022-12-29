dcdc5bebfc5d27774ff076ba5cd7e0bcf4cad4b2
hopefyjamshed <103117529+hopefyjamshed@users.noreply.github.com>
2022-12-29 21:54:14 +0600

    complete server added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const cors = require('cors');
 const express = require('express');
 const app = express()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const { query } = require('express');
 const port = process.env.PORT || 5000;
@@ -23,6 +23,9 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         const uploadCollection = client.db('notebook').collection('upload')
+        const commentCollection = client.db('notebook').collection('comment')
+        const profileCollection = client.db('notebook').collection('profile')
+
 
         // posting data to database
         app.post('/upload', async (req, res) => {
@@ -31,12 +34,50 @@ async function run() {
             res.send(result)
         })
 
+        // profile data 
+
+        app.post('/profile', async (req, res) => {
+            const query = req.body
+            const result = await profileCollection.insertOne(query)
+            res.send(result)
+        })
+
+        // getting profile data by email 
+
+        app.get('/profile/:email', async (req, res) => {
+            const email = req.params.email
+            const filter = { email: email }
+            const result = await profileCollection.find(filter).toArray()
+            res.send(result)
+        })
+
+        app.post('/comment', async (req, res) => {
+            const query = req.body
+            const result = await commentCollection.insertOne(query)
+            res.send(result)
+        })
+
         // get uplodet data from database 
         app.get('/uploaded', async (req, res) => {
             const query = {}
-            const result = await uploadCollection.find(query).toArray()
+            const result = await uploadCollection.find(query).sort({ '_id': -1 }).toArray()
             res.send(result)
         })
+
+        // getting uploaded data by id 
+        app.get('/uploaded/:id', async (req, res) => {
+            const id = req.params.id
+            const filter = { _id: ObjectId(id) }
+            const result = await uploadCollection.find(filter).toArray()
+            res.send(result)
+        })
+        // getting comment 
+        app.get('/comment', async (req, res) => {
+            const query = {}
+            const result = await commentCollection.find(query).toArray()
+            res.send(result)
+        })
+
 
     }
     finally {
